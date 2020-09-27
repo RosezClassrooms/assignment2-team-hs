f0ce14f80c3ea410cacd3cc2029dc92894c4f9e8
luke <luke@pop-os.localdomain>
2020-09-27 19:56:46 +0300

    added some comments

--- robot_refactored.py
+++ robot_refactored.py
@@ -1,138 +1,164 @@
-from abc import ABC, abstractmethod 
+from abc import ABC, abstractmethod
 
 class Robot:
 
-  def __init__(self, bipedal="", quadripedal= "", wheeled="", flying="", traversal=[], detection_systems=[]):
-    self.bipedal = bipedal
-    self.quadripedal = quadripedal
-    self.wheeled = wheeled
-    self.flying = flying
-    self.traversal = traversal
-    self.detection_systems = detection_systems
+    #Initialized values inside init to prevent the usage of flags. Using Flags inside the init function is bad practice
+    def __init__(self,
+                 bipedal="",
+                 quadripedal="",
+                 wheeled="",
+                 flying="",
+                 traversal=[],
+                 detection_systems=[]):
+        self.bipedal = bipedal
+        self.quadripedal = quadripedal
+        self.wheeled = wheeled
+        self.flying = flying
+        self.traversal = traversal
+        self.detection_systems = detection_systems
 
-  def __str__(self):
+    #Instead of printing by using multiple if statements with flags we decided to get the output directly from the builder thus eliminating the need for a lot of if statements
+    def __str__(self):
 
-    string = f"{self.bipedal}{self.quadripedal}{self.wheeled}{self.flying} ROBOT. \n"
-    if self.traversal:
-      string += "Traversal modules installed:\n"
-    for module in self.traversal:
-      string += "- " + str(module) + "\n"
-    if self.detection_systems:
-      string += "Detection systems installed:\n"
-    for system in self.detection_systems:
-      string += "- " + str(system) + "\n"
+        string = f"{self.bipedal}{self.quadripedal}{self.wheeled}{self.flying} ROBOT. \n"
+        if self.traversal:
+            string += "Traversal modules installed:\n"
+        for module in self.traversal:
+            string += "- " + str(module) + "\n"
+        if self.detection_systems:
+            string += "Detection systems installed:\n"
+        for system in self.detection_systems:
+            string += "- " + str(system) + "\n"
 
-    return string
+        return string
+
 
 class BipedalLegs:
-  def __str__(self):
-    return "two legs"
+    def __str__(self):
+        return "two legs"
+
 
 class QuadripedalLegs:
-  def __str__(self):
-    return "four legs"
+    def __str__(self):
+        return "four legs"
+
 
 class Arms:
-  def __str__(self):
-    return "two arms"
+    def __str__(self):
+        return "two arms"
+
 
 class Wings:
-  def __str__(self):
-    return "wings"
+    def __str__(self):
+        return "wings"
+
 
 class Blades:
-  def __str__(self):
-    return "blades"
+    def __str__(self):
+        return "blades"
+
 
 class FourWheels:
-  def __str__(self):
-    return "four wheels"
+    def __str__(self):
+        return "four wheels"
+
 
 class TwoWheels:
-  def __str__(self):
-    return "two wheels"
+    def __str__(self):
+        return "two wheels"
+
 
 class CameraDetectionSystem:
-  def __str__(self):
-    return "cameras"
+    def __str__(self):
+        return "cameras"
+
 
 class InfraredDetectionSystem:
-  def __str__(self):
-    return "infrared"
+    def __str__(self):
+        return "infrared"
 
 
+#Moved the init and reset function inside the builder class because they are common among other builder and have no need for reinitialization
 class RobotBuilder(ABC):
-    
-  def __init__(self):
-    self.product = Robot()
+    def __init__(self):
+        self.product = Robot()
 
-  def reset(self):
-    self.product = Robot()
+    def reset(self):
+        self.product = Robot()
 
-  @abstractmethod
-  def build_traversal(self):
-    pass
+    @abstractmethod
+    def build_traversal(self):
+        pass
 
-  @abstractmethod
-  def build_detection_system(self):
-    pass
+    @abstractmethod
+    def build_detection_system(self):
+        pass
 
-  @abstractmethod
-  def set_type(self):
-    pass
+    #added a setter method for naming the robot so that we could get rid of all the if statements in the robot class
+    @abstractmethod
+    def set_type(self):
+        pass
 
-  def get_product(self):
-    return self.product
-    
+    def get_product(self):
+        return self.product
+
+
 # Concrete Builder class:  there would be MANY of these
 class AndroidBuilder(RobotBuilder):
-  
-  def set_type(self):
-    self.product.bipedal = "BIPEDAL"
 
-  def build_traversal(self):
-    self.product.traversal.clear()
-    self.product.detection_systems.clear()
-    self.product.traversal.append(BipedalLegs())
-    self.product.traversal.append(Arms())
-    
-  def build_detection_system(self):
-    self.product.detection_systems.append(CameraDetectionSystem())
-    
+    #sets the type of the robot
+    def set_type(self):
+        self.product.bipedal = "BIPEDAL"
+
+    #added clear functions to prevent double printing
+    def build_traversal(self):
+        self.product.traversal.clear()
+        self.product.detection_systems.clear()
+        self.product.traversal.append(BipedalLegs())
+        self.product.traversal.append(Arms())
+
+    def build_detection_system(self):
+        self.product.detection_systems.append(CameraDetectionSystem())
+
+
 # Concrete Builder class:  there would be many of these
 class AutonomousCarBuilder(RobotBuilder):
 
-  def set_type(self):
-    self.product.wheeled = "WHEELED"
+    #sets the type of the robot
+    def set_type(self):
+        self.product.wheeled = "WHEELED"
 
-  def build_traversal(self):
-    self.product.traversal.clear()
-    self.product.detection_systems.clear()
-    self.product.traversal.append(FourWheels())
-    
-  def build_detection_system(self):
-    self.product.detection_systems.append(InfraredDetectionSystem())
-    
+    #added clear functions to prevent double printing
+    def build_traversal(self):
+        self.product.traversal.clear()
+        self.product.detection_systems.clear()
+        self.product.traversal.append(FourWheels())
+
+    def build_detection_system(self):
+        self.product.detection_systems.append(InfraredDetectionSystem())
+
 
 #new type of Robot
 class FlyingBattleBot(RobotBuilder):
 
-  def set_type(self):
-    self.product.flying = "FLYING"
+    #sets the type of the robot
+    def set_type(self):
+        self.product.flying = "FLYING"
 
-  def build_traversal(self):
-    self.product.traversal.clear()
-    self.product.detection_systems.clear()
-    self.product.traversal.append(TwoWheels())
-    self.product.traversal.append(Wings())
-    self.product.traversal.append(Blades())
-    
+    #added clear functions to prevent double printing
+    def build_traversal(self):
+        self.product.traversal.clear()
+        self.product.detection_systems.clear()
+        self.product.traversal.append(TwoWheels())
+        self.product.traversal.append(Wings())
+        self.product.traversal.append(Blades())
 
-  def build_detection_system(self):
-    self.product.detection_systems.append(InfraredDetectionSystem())
-    self.product.detection_systems.append(CameraDetectionSystem())
-  
+    def build_detection_system(self):
+        self.product.detection_systems.append(InfraredDetectionSystem())
+        self.product.detection_systems.append(CameraDetectionSystem())
 
+
+#merged the make functions to a single one because the function they used were common
 class Director:
     def make_robot(self, builder):
         builder.set_type()
@@ -140,17 +166,21 @@ class Director:
         builder.build_detection_system()
         return builder.get_product()
 
+
+#put the function calls inside main function for design purposes
 def main():
-  director = Director()
+    director = Director()
 
-  builder = AndroidBuilder()
-  print(director.make_robot(builder))
+    builder = AndroidBuilder()
+    print(director.make_robot(builder))
 
-  builder = AutonomousCarBuilder()
-  print(director.make_robot(builder))
+    builder = AutonomousCarBuilder()
+    print(director.make_robot(builder))
 
-  builder = FlyingBattleBot()
-  print(director.make_robot(builder))
+    builder = FlyingBattleBot()
+    print(director.make_robot(builder))
+
 
 if __name__ == "__main__":
-  main()
+    main()
+
